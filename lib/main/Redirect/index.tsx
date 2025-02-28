839df1b6e4c25fcb3f7f5a983e1b3c73d5f2c8d7
wikiakai <59285719+wikiakai@users.noreply.github.com>
2025-02-28 21:53:21 +0900

    Update index.tsx

--- lib/main/Redirect/index.tsx
+++ lib/main/Redirect/index.tsx
@@ -1,16 +1,29 @@
 'use client'
 import GlassCard from '@/components/GlassCard'
-import { Box, Button, Dialog, Typography } from '@mui/material'
+import { Box, Button, Dialog, Typography,  MenuItem, Select, Typography } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
 const RedirectPage = () => {
   const router = useRouter()
   const [open, setOpen] = useState(true)
+   const [counter, setCounter] = useState(0);
+  const [limit, setLimit] = useState(50);
+
   const handleRedirect = () => {
     setOpen(false)
     router.push('/login')
   }
+
+  const handleClick = (event) => {
+  
+        window.open("https://www.effectiveratecpm.com/qyszu9ib?key=d2631095e677c94c8449218b2aedb31b", "_blank");
+    setCounter((prev) => {
+      const newCount = prev + 1;
+      return newCount;
+    });
+  };
+  
   return (
     <Box
       sx={{
@@ -25,9 +38,20 @@ const RedirectPage = () => {
     >
       <GlassCard>
         <Typography sx={{ mb: '24px' }}>Please login first!</Typography>
-        <Button onClick={handleRedirect} variant="contained">
+        <Button onClick={handleClick} variant="contained">
           Login
         </Button>
+        <Select
+        value={limit}
+        onChange={(e) => setLimit(parseInt(e.target.value, 10))}
+        style={{ marginTop: "10px" }}
+      >
+        {[50, 80, 100, 120].map((value) => (
+          <MenuItem key={value} value={value}>
+            {value}
+          </MenuItem>
+        ))}
+      </Select>
       </GlassCard>
     </Box>
   )
